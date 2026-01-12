14daf6cd5d01115182af8deadd619466afe5fcea
AtelierMizumi <AtelierMizumi@users.noreply.github.com>
2026-01-12 05:43:39 +0000

    deploy: 56a5dc0f11fe1ad49a947b3b0c5c32ecf6ba9de4

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3860930067"
+    serviceWorkerVersion: "2594931449"
   }
 });
